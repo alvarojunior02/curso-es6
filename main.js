8c2aa8a0780b555b8ee46d87c5487d08e7881e6c
alvarojunior02 <alvaroamcjunior8@gmail.com>
2020-09-20 14:00:19 -0400

    Template literals

--- main.js
+++ main.js
@@ -1,49 +1,7 @@
-// REST
+// TEMPLATE LITERALS
 
-console.log('------ REST ------');
+const nome = "Alvaro";
+const idade = 18;
 
-const usuario = {
-    nome: 'Alvaro',
-    idade: 18,
-    empresa: 'IFMT'
-};
-
-const { nome, ...resto } = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function soma(...params) {
-    return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(1, 3, 4));
-
-// SPREAD
-
-console.log('------ SPREAD ------');
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [ ...arr1, ...arr2 ];
-
-console.log(arr3);
-
-const usuario1 = {
-    nome: 'Alvaro',
-    idade: 18,
-    empresa: 'IFMT',
-}
-
-const usuario2 = { ...usuario1, nome: 'Alvaro Junior'};
-
-console.log(usuario2);
+//console.log("Meu nome é " + nome + " e tenho " + idade + " anos");
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
