9882548f428f6e152bf86502000580425f3918b7
alvarojunior02 <alvaroamcjunior8@gmail.com>
2020-09-20 14:47:27 -0400

    Desafio 1 - Exercicio 5

--- bundle.js
+++ bundle.js
@@ -1,28 +1,53 @@
 "use strict";
 
-// 4.1
-var empresa = {
-  nome: 'Rocketseat',
-  endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC'
-  }
-};
-var nome = empresa.nome,
-    _empresa$endereco = empresa.endereco,
-    cidade = _empresa$endereco.cidade,
-    estado = _empresa$endereco.estado;
-console.log(nome);
-console.log(cidade);
-console.log(estado); // 4.2
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return "".concat(nome, " tem ").concat(idade, " anos.");
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// 5.1
+var arr = [1, 2, 3, 4, 5, 6];
+var x = arr[0],
+    y = arr.slice(1);
+console.log(x);
+console.log(y);
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-mostraInfo({
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+
+console.log(soma(1, 2)); // 3
+// 5.2
+
+var usuario = {
   nome: 'Alvaro',
-  idade: 18
+  idade: 18,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+};
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario), {}, {
+  nome: 'Gabriel'
 });
+
+console.log(usuario2);
+
+var usuario3 = _objectSpread(_objectSpread({}, usuario), {}, {
+  endereco: _objectSpread(_objectSpread({}, usuario.endereco), {}, {
+    cidade: "Lontras"
+  })
+});
+
+console.log(usuario3);
--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-// 4.1
-const empresa = {
-    nome: 'Rocketseat',
+// 5.1
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+const [x, ...y] = arr;
+
+console.log(x);
+console.log(y);
+
+function soma(...params) {
+    return params.reduce((total, next) => total + next);
+}
+
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+console.log(soma(1, 2)); // 3
+
+// 5.2
+
+const usuario = {
+    nome: 'Alvaro',
+    idade: 18,
     endereco: {
         cidade: 'Rio do Sul',
-        estado: 'SC',
+        uf: 'SC',
+        pais: 'Brasil',
     }
 };
 
-const { nome, endereco: { cidade, estado }} = empresa;
+const usuario2 = {...usuario, nome: 'Gabriel'}
+console.log(usuario2);
 
-console.log(nome);
-console.log(cidade);
-console.log(estado);
-
-// 4.2
-function mostraInfo({ nome, idade }) {
-    return `${nome} tem ${idade} anos.`;
-}
-
-mostraInfo({ nome: 'Alvaro', idade: 18 })
+const usuario3 = {...usuario, endereco: {...usuario.endereco, cidade: "Lontras"}};
+console.log(usuario3);
