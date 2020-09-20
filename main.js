d10542c3f48544c6a6e2a2dcdd9a84db8de36923
alvarojunior02 <alvaroamcjunior8@gmail.com>
2020-09-20 13:39:29 -0400

    Desestruturação

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-const soma = (a = 3, b = 6) => a + b;
+const usuario = {
+    nome: 'Alvaro',
+    idade: 18,
+    endereco: {
+        cidade: 'Cuiaba',
+        estado: 'MT'
+    }
+}
 
-console.log(soma(1));
-console.log(soma());
+const { nome, idade, endereco: { cidade }} = usuario;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostarNome({ nome, idade }) {
+    console.log(nome, idade);
+}
+
+mostarNome(usuario);
